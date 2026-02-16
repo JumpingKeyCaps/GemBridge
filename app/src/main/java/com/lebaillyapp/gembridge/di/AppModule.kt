81f36d34cda1eb68bf6a2385acdb6809668509da
Samuel Lebailly <sam.meusa@gmail.com>
2026-02-16 18:22:42 +0100

    feature(di/): Update the AppModule with generative model provider (used by the service) and a provider of the new service (used by the repository), all provider are obviously singleton.

--- app/src/main/java/com/lebaillyapp/gembridge/di/AppModule.kt
+++ app/src/main/java/com/lebaillyapp/gembridge/di/AppModule.kt
@@ -1,8 +1,11 @@
 package com.lebaillyapp.gembridge.di
 
+import com.google.ai.client.generativeai.GenerativeModel
+import com.lebaillyapp.gembridge.BuildConfig
 import com.lebaillyapp.gembridge.data.repository.GeminiRepository
 import com.lebaillyapp.gembridge.data.service.GeminiService
-import com.lebaillyapp.gembridge.data.service.MockGeminiService
+import com.lebaillyapp.gembridge.data.service.generativeAi.GoogleGeminiService
+import com.lebaillyapp.gembridge.data.service.mock.MockGeminiService
 import com.lebaillyapp.gembridge.domain.usecase.GetChatResponseUseCase
 import dagger.Module
 import dagger.Provides
@@ -14,6 +17,7 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class) // Le module vit aussi longtemps que l'app
 object AppModule {
 
+    //MODE 0 - Mock service (used to debug app architecture pipeline)
     /**
      * Fournit l'implémentation concrète du service.
      * Pour l'instant on retourne le Mock.
@@ -24,6 +28,23 @@ object AppModule {
         return MockGeminiService() // on utilise notre mock pour le moment !
     }
 
+    // MODE 2 - GenerativeAI Google SDK
+    @Provides
+    @Singleton
+    fun provideGenerativeModel(): GenerativeModel {
+        // Configuration du moteur Gemini
+        return GenerativeModel(
+            modelName = "gemini-1.5-flash", // Version rapide et efficace pour un PoC
+            apiKey = BuildConfig.GEMINI_API_KEY
+        )
+    }
 
+    @Provides
+    @Singleton
+    fun provideGeminiService(generativeModel: GenerativeModel): GeminiService {
+        // Pour l'instant, on branche l'implémentation Google.
+        // Plus tard, on pourra switcher ici sur Retrofit ou Firebase.
+        return GoogleGeminiService(generativeModel)
+    }
 
 }
